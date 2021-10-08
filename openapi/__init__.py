0480042be44edae0519234ba7080a93f2286e081
liuzhichao <2904662085@qq.com>
2021-10-08 19:33:51 +0800

    chore: init files

--- openapi/__init__.py
+++ openapi/__init__.py
@@ -1,3 +1,19 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# Date: 2021/10/8
+r"""
+ ______     ______   ______     __   __     ______     ______   __
+/\  __ \   /\  == \ /\  ___\   /\ "-.\ \   /\  __ \   /\  == \ /\ \
+\ \ \/\ \  \ \  _-/ \ \  __\   \ \ \-.  \  \ \  __ \  \ \  _-/ \ \ \
+ \ \_____\  \ \_\    \ \_____\  \ \_\\"\_\  \ \_\ \_\  \ \_\    \ \_\
+  \/_____/   \/_/     \/_____/   \/_/ \/_/   \/_/\/_/   \/_/     \/_/
+
+"""
+
+VERSION = (0, 0, 1, "final", 0)
+
+__title__ = "openapi"
+__version_info__ = VERSION
+__version__ = ".".join(map(str, VERSION[:3])) + (
+    "-{}{}".format(VERSION[3], VERSION[4] or "") if VERSION[3] != "final" else ""
+)
+__author__ = "ZhiChao Liu"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021"
