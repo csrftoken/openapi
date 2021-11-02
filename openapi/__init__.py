b5bdaeea3df08b6944fa6ab8ab39a9a2d72d9824
liuzhichao <2904662085@qq.com>
2021-11-02 23:16:12 +0800

    chore: 双引号 -> 单引号

--- openapi/__init__.py
+++ openapi/__init__.py
@@ -7,13 +7,14 @@ r"""
 
 """
 
-VERSION = (0, 0, 1, "final", 0)
+VERSION = (0, 0, 1, 'final', 0)
 
-__title__ = "openapi"
+__title__ = 'openapi'
 __version_info__ = VERSION
-__version__ = ".".join(map(str, VERSION[:3])) + (
-    "-{}{}".format(VERSION[3], VERSION[4] or "") if VERSION[3] != "final" else ""
+__version__ = ''.join(map(str, VERSION[:3])) + (
+    '-{}{}'.format(VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else ''
 )
-__author__ = "ZhiChao Liu"
-__license__ = "MIT"
-__copyright__ = "Copyright 2021"
+__author__ = 'ZhiChao Liu'
+__license__ = 'MIT'
+__email__ = 'liuzhichao@gamil.com'
+__copyright__ = 'Copyright 2021'
---
+++ openapi/providers/alipay/pay/__init__.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Date: 2021/11/2
